3726018313fd8aa4b4aca2d288fb60be46c3747d
Mariam Tandashvili <73125781+mari839@users.noreply.github.com>
2023-06-04 18:19:36 +0400

    Update ShoppingCartViewModel.cs

--- BethanyPieShop/ViewModels/ShoppingCartViewModel.cs
+++ BethanyPieShop/ViewModels/ShoppingCartViewModel.cs
@@ -3,9 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BethanyPieShop.ViewModels
 {
-    public class ShoppingCartViewModel //sxvanairad rato vqenit DI
+    public class ShoppingCartViewModel 
     {
-        public IShoppingCart ShoppingCart { get; } //amas rom gadavcemt rat gvinda model ar gvinda?
+        public IShoppingCart ShoppingCart { get; } 
         public decimal ShoppingCartTotal { get;}
 
         public ShoppingCartViewModel(IShoppingCart shoppingCart, decimal shoppingCartTotal) 
